118c2fa7ec9bc2a1f91e27cfa60dae7cbfb2e1b4
Ben Thomasson <bthomass@redhat.com>
2023-03-09 17:36:26 -0500

    Add simple schema

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,18 +1,32 @@
-
 import os
 import yaml
-from pprint import pprint
+import json
+from jsonschema import validate
 
 HERE = os.path.dirname(os.path.abspath(__file__))
-print(HERE)
+
 
 def load_blueprint(name):
     with open(os.path.join(HERE, "blueprints", name, "resources.yml")) as f:
         return yaml.safe_load(f.read())
 
 
+def load_schema(name):
+    with open(
+        os.path.join(HERE, "blueprints", name, "resource_schema.json")
+    ) as f:
+        return json.loads(f.read())
+
+
+def validate_blueprint(name):
+    blueprint = load_blueprint(name)
+    blueprint_schema = load_schema(name)
+    validate(blueprint, blueprint_schema)
+
+
 def test_hello_world():
-    print(HERE)
-    blueprint = load_blueprint('hello_world')
-    pprint(blueprint)
-    assert blueprint['resources'][0]['hello_world']['message'] == 'Hello World!'
+    blueprint = load_blueprint("hello_world")
+    assert (
+        blueprint["resources"][0]["hello_world"]["message"] == "Hello World!"
+    )
+    assert validate_blueprint("hello_world") is None
