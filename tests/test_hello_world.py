4c2be1ca6b66025e1e4cff028088702f11aeaba2
Ben Thomasson <bthomass@redhat.com>
2023-03-09 17:23:08 -0500

    Add test

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,4 +1,18 @@
 
+import os
+import yaml
+from pprint import pprint
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+print(HERE)
+
+def load_blueprint(name):
+    with open(os.path.join(HERE, "blueprints", name, "resources.yml")) as f:
+        return yaml.safe_load(f.read())
+
 
 def test_hello_world():
-    assert False
+    print(HERE)
+    blueprint = load_blueprint('hello_world')
+    pprint(blueprint)
+    assert blueprint['resources'][0]['hello_world']['message'] == 'Hello World!'
